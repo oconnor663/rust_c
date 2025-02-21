7584bd191b94a655b5bcf37b9f9fa688293d74a3
Jack O'Connor <oconnor663@gmail.com>
2025-02-20 17:10:30 -0800

    get rid of another unwrap

--- c_vec/build.rs
+++ c_vec/build.rs
@@ -5,7 +5,7 @@ fn main() -> anyhow::Result<()> {
     let bindings = bindgen::Builder::default()
         .header("src/c_vec.h")
         .generate()?;
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_path = PathBuf::from(env::var("OUT_DIR")?);
     bindings.write_to_file(out_path.join("bindings.rs"))?;
 
     cc::Build::new().file("src/c_vec.c").compile("c_vec");
