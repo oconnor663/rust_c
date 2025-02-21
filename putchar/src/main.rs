95016f8bfa5d4b5eb741c326050a9821b1199ee0
Jack O'Connor <oconnor663@gmail.com>
2025-02-20 16:56:10 -0800

    correct the signature of putchar()

--- putchar/src/main.rs
+++ putchar/src/main.rs
@@ -1,13 +1,13 @@
-use std::ffi::c_char;
+use std::ffi::c_int;
 
 unsafe extern "C" {
-    fn putchar(c: c_char);
+    fn putchar(c: c_int) -> c_int;
 }
 
 fn main() {
     for &byte in b"hello world\n" {
         unsafe {
-            putchar(byte as c_char);
+            putchar(byte as c_int);
         }
     }
 }
