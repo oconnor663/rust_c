27eaac85caff1f0e6f24e08dfec246cd6983bf2d
Jack O'Connor <oconnor663@gmail.com>
2025-04-22 11:44:37 -0700

    simplify linked_list

--- linked_list/src/linked_list.c
+++ linked_list/src/linked_list.c
@@ -1,66 +1,21 @@
-#include <assert.h>
 #include <stdlib.h>
 
 #include "linked_list.h"
 
-void linked_list_init(linked_list *self) {
-    self->head = NULL;
-    self->tail = NULL;
+void linked_list_push(node **list, void *elem) {
+    node *new_node = (node *)malloc(sizeof(node));
+    new_node->elem = elem;
+    new_node->next = *list;
+    *list = new_node;
 }
 
-size_t linked_list_len(const linked_list *self) {
-    size_t len = 0;
-    node *cursor = self->head;
-    while (cursor != NULL) {
-        len++;
-        cursor = cursor->next;
-    }
-    return len;
-}
-
-void linked_list_push(linked_list *self, void *elem) {
-    node *new_tail = malloc(sizeof(node));
-    new_tail->elem = elem;
-    new_tail->next = NULL;
-    new_tail->prev = self->tail;
-    if (self->tail != NULL) {
-        assert(self->head != NULL);
-        self->tail->next = new_tail;
-    } else {
-        assert(self->head == NULL);
-        self->head = new_tail;
-    }
-    self->tail = new_tail;
-}
-
-void *linked_list_pop(linked_list *self) {
-    if (self->tail == NULL) {
-        assert(self->head == NULL);
+void *linked_list_pop(node **list) {
+    node *head = *list;
+    if (head == NULL) {
         return NULL;
     }
-    assert(self->head != NULL);
-    void *ret = self->tail->elem;
-    node *new_tail = self->tail->prev;
-    if (new_tail != NULL) {
-        new_tail->next = NULL;
-    } else {
-        self->head = NULL;
-    }
-    self->tail = new_tail;
-    return ret;
-}
-
-void linked_list_concat(linked_list *self, linked_list *other) {
-    if (self->head == NULL) {
-        *self = *other;
-        linked_list_init(other);
-        return;
-    }
-    if (other->head == NULL) {
-        return;
-    }
-    self->tail->next = other->head;
-    other->head->prev = self->tail;
-    self->tail = other->tail;
-    linked_list_init(other);
+    void *elem = head->elem;
+    *list = head->next;
+    free(head);
+    return elem;
 }
--- linked_list/src/linked_list.h
+++ linked_list/src/linked_list.h
@@ -1,23 +1,15 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
-#include <stddef.h>
+typedef struct node node;
 
-typedef struct node {
+struct node {
     void *elem;
-    struct node *next;
-    struct node *prev;
-} node;
+    node *next;
+};
 
-typedef struct linked_list {
-    node *head;
-    node *tail;
-} linked_list;
-
-void linked_list_init(linked_list *self);
-size_t linked_list_len(const linked_list *self);
-void linked_list_push(linked_list *self, void *elem);
-void *linked_list_pop(linked_list *self);
-void linked_list_concat(linked_list *self, linked_list *other);
+// In Rust terms we could say that `list` is a `&mut Option<Box<Node>>`.
+void linked_list_push(node **list, void *elem);
+void *linked_list_pop(node **list);
 
 #endif
